325170e2497e31ab46303edf1f82141960c1e591
tuck-2 <taikisakai936@gmail.com>
2021-04-13 11:58:48 +0000

    [Add]要求要件

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,52 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+    @book =Book.new
+  end
+
+  def new
+
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:success] = 'Book was successfully created'
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render "index"
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:success] = "Book was successfully edited"
+      redirect_to book_path(book)
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+      flash[:success] = "Book was successfully destroyed"
+      redirect_to books_path
+    end
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
